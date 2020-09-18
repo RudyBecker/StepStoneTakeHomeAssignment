9250e0288c312df0b8eed261ef13c420506d43bb
RudyBecker <52896529+RudyBecker@users.noreply.github.com>
2020-09-18 11:50:41 -0700

    First Commit

--- Assignment.js
+++ Assignment.js
@@ -8,6 +8,6 @@ function palindrome(str) {
   }
   console.log(palindrome("A man, a plan, a canal. Panama"));
 
-//   https://www.freecodecamp.org/news/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7/
+
 
 // 2. Write a function that checks if an inputted value is a numerical range "100-200". Inputted values can be an integer or a string in the previously stated format. The return should be a true/false (bool) value. Ranges should also allow floats (e.g. "100.00"). The range should also be listed as min/max order. Valid values: 100-200, 100.0-200.1. Invalid Values: 100, 200-100.
